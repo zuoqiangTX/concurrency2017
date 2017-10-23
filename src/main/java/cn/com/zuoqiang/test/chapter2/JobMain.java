034dd1da91d65877485944fb943ca5e0ac79243a
zuoqiang <756465185@qq.com>
2017-10-23 22:55:01 +0800

    使用锁实现同步 如果线程不能得到Lock就会先休眠，同一时间只有一个线程才能访问Lock之间的临界区。

---
+++ src/main/java/cn/com/zuoqiang/test/chapter2/JobMain.java
@@ -0,0 +1,14 @@
+package cn.com.zuoqiang.test.chapter2;
+
+public class JobMain {
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new Job(printQueue), "Thread" + i);
+        }
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+        }
+    }
+}
---
+++ src/main/java/cn/com/zuoqiang/test/chapter2/PrintQueue.java
@@ -0,0 +1,21 @@
+package cn.com.zuoqiang.test.chapter2;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrintQueue {
+    private final Lock queueLock = new ReentrantLock();
+
+    public void printJob(Object document) {
+        queueLock.lock();
+        try {
+            long duration = (long) (Math.random() * 10000);
+            System.out.println(Thread.currentThread().getName() + ": PrintQueue :Printing a job during " + (duration / 1000) + "seconds");
+            Thread.sleep(duration);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+}
