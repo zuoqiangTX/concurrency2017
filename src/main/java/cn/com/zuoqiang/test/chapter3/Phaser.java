4f247e0dc8039a734daef4f60c76834b8e82470d
zuoqiang <756465185@qq.com>
2017-10-24 21:28:42 +0800

    并发阶段任务的运行，允许执行多阶段并发任务【暂时不需要了解】

---
+++ src/main/java/cn/com/zuoqiang/test/chapter3/Phaser.java
@@ -0,0 +1,7 @@
+package cn.com.zuoqiang.test.chapter3;
+
+public class Phaser {
+    public static void main(String[] args) {
+        System.out.println("这个适用于在每一步结束的时候对线程进行同步，当所有的线程都完成这一步的时候，才允许执行下一步");
+    }
+}
