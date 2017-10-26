689b388d9c3d47ab0f056ec04af7caef6cdbd3f7
zuoqiang <756465185@qq.com>
2017-10-26 20:14:08 +0800

    ConcurrentLinkedDeque 【非阻塞线程安全列表】 //非阻塞线程安全列表，添加或移除数据的时候，如果不能立即执行，则返回null或者抛出异常，调用方法的线程不会阻塞。（这点与 阻塞线程安全列表相反）

---
+++ src/main/java/cn/com/zuoqiang/test/chapter6/AddTask.java
@@ -0,0 +1,8 @@
+package cn.com.zuoqiang.test.chapter6;
+
+public class AddTask implements Runnable {
+    //private ConcurrentLinkedDeque<String> list;   //非阻塞线程安全列表，添加或移除数据的时候，如果不能立即执行，则返回null或者抛出异常，调用方法的线程不会阻塞。
+    public void run() {
+        //TODO jdk未升级，暂时无法写，1.7+才支持。
+    }
+}
